12f5d6e1dbe9d8df14388999f9f4b94276196d87
RudraRajbanshi <rudra.raj.716@gmail.com>
2019-04-25 20:59:16 +0545

    navigation drawer created for admin

--- app/src/main/java/com/newsportal/MainActivity.java
+++ app/src/main/java/com/newsportal/MainActivity.java
@@ -1,13 +1,37 @@
 package com.newsportal;
 
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 
 public class MainActivity extends AppCompatActivity {
+    private NavigationView navBar;
+    private Toolbar toolbar;
+    private DrawerLayout drawerLayout;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        navBar = findViewById(R.id.navBar);
+        drawerLayout = findViewById(R.id.drawerLayout);
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBarDrawerToggle mDrawerToggle = new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(mDrawerToggle);
+
+        mDrawerToggle.syncState();
+//
+//        hidedrawermenu();
     }
+//    private void hidedrawermenu(){
+//        Menu menu = navBar.getMenu();
+//        menu.findItem(R.id.navBar).setVisible(false);
+//    }
 }
